fab6f198abd2fd01882c18459f9e781924e26fde
Hazem020 <hazemhamed34@gmail.com>
2023-09-26 22:32:40 +0300

    edited compression

--- app.js
+++ app.js
@@ -60,7 +60,17 @@ app.use(
   })
 );
 
-app.use(compression);
+app.use(compression({ filter: shouldCompress }));
+
+function shouldCompress(req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
 
 //read data from body into req.body
 app.use(express.json({ limit: '10kb' }));
