3a37ac0fe4d64a3d6831791d10c162d627c37749
Hazem020 <hazemhamed34@gmail.com>
2023-09-27 00:48:51 +0300

    edited cookie method

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,13 +9,12 @@ const createAndSendToken = (user, code, req, res) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_KEY, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
   };
 
   res.cookie('jwt', token, cookieOptions);
