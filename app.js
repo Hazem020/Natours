9726c3a25de08cefad63ea2b0510396e1b091437
Hazem020 <hazemhamed34@gmail.com>
2023-09-26 22:36:25 +0300

    edited compression

--- app.js
+++ app.js
@@ -60,13 +60,13 @@ app.use(
   })
 );
 
-app.use(compression({ filter: shouldCompress }));
+// app.use(compression({ filter: shouldCompress }));
 
-function shouldCompress(req, res) {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header
-    return false;
-  }
+// function shouldCompress(req, res) {
+//   if (req.headers['x-no-compression']) {
+//     // don't compress responses with this request header
+//     return false;
+//   }
 
   // fallback to standard filter function
   return compression.filter(req, res);
